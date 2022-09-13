f9fbac0806695213d4b64942f676a6575d900d66
waynetipsy <jacketoniru@gmail.com>
2022-09-13 11:53:57 +0100

    update app

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val weightText = findViewById<EditText>(R.id.etWeight)
+        val heightText = findViewById<EditText>(R.id.etHeight)
+        val calcButton = findViewById<Button>(R.id.btmCalculate)
+
+        calcButton.setOnClickListener {
+            val weight = weightText.text.toString()
+            val height = heightText.text.toString()
+            val bmi = weight.toFloat()/((height.toFloat()/100)*(height.toFloat()/100))
+          // get result with two decimal places
+        }
     }
 }
